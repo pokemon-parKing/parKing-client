6278a9392248145fb7401b5c1840e123372f1aad
stevkim <stevenn.kkim@gmail.com>
2023-12-22 14:28:05 -0800

    Added our color scheme to tailwind config.

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,18 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "black-p": "var(--pk-black)",
+        "black-s": "var(--pk-black-secondary)",
+        "burgundy-p": "var(--pk-burgundy)",
+        "burgundy-s": "var(--pk-burgundy-secondary)",
+        "white-p": "var(--pk-white)",
+        "white-s": "var(--pk-white-secondary)",
+        "beige-p": "var(--pk-beige)",
+        "beige-s": "var(--pk-beige-secondary)",
+      }
+    },
   },
   plugins: [import("daisyui")],
 }
-
