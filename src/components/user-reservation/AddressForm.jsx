3f9f701fd48c15aab895eb5d4aa0ddac5e80f209
Christian Lee <christianlee4037@gmail.com>
2023-12-26 13:12:33 -0800

    Updated reservation styling to match figma

--- src/components/user-reservation/AddressForm.jsx
+++ src/components/user-reservation/AddressForm.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import react, { useState } from "react";
 const AddressForm = () => {
   const [address, setAddress] = useState({});
   const handleSubmit = (e) => {
@@ -6,75 +6,83 @@ const AddressForm = () => {
     console.log(address);
   };
   return (
-    <form onSubmit={handleSubmit}>
-      <label htmlFor="street">
-        Street Address{" "}
-        <input
-          type="text"
-          onChange={(e) => {
-            const updatedAddress = address;
-            updatedAddress.street = e.target.value;
-            setAddress(updatedAddress);
-          }}
-        />
-      </label>
-      <label htmlFor="apt">
-        Apt/Unit #{" "}
-        <input
-          type="text"
-          onChange={(e) => {
-            const updatedAddress = address;
-            updatedAddress.apt = e.target.value;
-            setAddress(updatedAddress);
-          }}
-        />
-      </label>
-      <label htmlFor="zip">
-        Zip Code{" "}
-        <input
-          type="number"
-          onChange={(e) => {
-            const updatedAddress = address;
-            updatedAddress.zip = e.target.value;
-            setAddress(updatedAddress);
-          }}
-        />
-      </label>
-      <label htmlFor="city">
-        City{" "}
-        <input
-          type="text"
-          onChange={(e) => {
-            const updatedAddress = address;
-            updatedAddress.city = e.target.value;
-            setAddress(updatedAddress);
-          }}
-        />
-      </label>
-      <label htmlFor="state">
-        State{" "}
-        <input
-          type="text"
-          onChange={(e) => {
-            const updatedAddress = address;
-            updatedAddress.state = e.target.value;
-            setAddress(updatedAddress);
-          }}
-        />
-      </label>
-      <label htmlFor="country">
-        Country{" "}
-        <input
-          type="text"
-          onChange={(e) => {
-            const updatedAddress = address;
-            updatedAddress.country = e.target.value;
-            setAddress(updatedAddress);
-          }}
-        />
-      </label>
-      <button type="submit">Submit</button>
-    </form>
+    <div className="flex justify-center">
+      <form onSubmit={handleSubmit} className="w-1/2">
+        <div className="flex flex-row">
+          <div className="flex flex-col w-3/4">
+            <label htmlFor="street">Street Address </label>
+            <input
+              type="text"
+              onChange={(e) => {
+                const updatedAddress = address;
+                updatedAddress.street = e.target.value;
+                setAddress(updatedAddress);
+              }}
+            />
+          </div>
+          <div className="flex flex-col w-1/4">
+            <label htmlFor="apt">Apt/Unit # </label>
+            <input
+              type="text"
+              onChange={(e) => {
+                const updatedAddress = address;
+                updatedAddress.apt = e.target.value;
+                setAddress(updatedAddress);
+              }}
+            />
+          </div>
+        </div>
+        <div className="flex flex-row">
+          <div className="flex flex-col w-1/2">
+            <label htmlFor="zip">Zip Code </label>
+            <input
+              type="number"
+              onChange={(e) => {
+                const updatedAddress = address;
+                updatedAddress.zip = e.target.value;
+                setAddress(updatedAddress);
+              }}
+            />
+          </div>
+          <div className="flex flex-col w-1/2">
+            <label htmlFor="city">City </label>
+            <input
+              type="text"
+              onChange={(e) => {
+                const updatedAddress = address;
+                updatedAddress.city = e.target.value;
+                setAddress(updatedAddress);
+              }}
+            />
+          </div>
+        </div>
+        <div className="flex flex-row">
+          <div className="flex flex-col w-1/2">
+            <label htmlFor="state">State </label>
+            <input
+              type="text"
+              onChange={(e) => {
+                const updatedAddress = address;
+                updatedAddress.state = e.target.value;
+                setAddress(updatedAddress);
+              }}
+            />
+          </div>
+          <div className="flex flex-col w-1/2">
+            <label htmlFor="country">Country </label>
+            <input
+              type="text"
+              onChange={(e) => {
+                const updatedAddress = address;
+                updatedAddress.country = e.target.value;
+                setAddress(updatedAddress);
+              }}
+            />
+          </div>
+        </div>
+        <button type="submit">Submit</button>
+      </form>
+    </div>
   );
 };
 
