4bd8e222d7afad1f1efc2175ee03bb61cd189d1d
Christian Lee <christianlee4037@gmail.com>
2023-12-27 11:10:57 -0800

    Added routes to user reservation page

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,14 @@
 import UserReservation from "./containers/UserReservation.jsx";
-// import { Link } from 'react-router-dom';
-import dotenv from "dotenv";
+import { Link } from "react-router-dom";
 
 function App() {
   return (
-    <>
-      {/* <Link to={'/pokemon'}>Pokemon</Link> */}
+    <div className="flex flex-col">
       parKing - Pokemon
       <div className="flex flex-col md:flex-row ">Test</div>
-      <UserReservation />
-    </>
+      <Link to={"/pokemon"}>Pokemon</Link>
+      <Link to={"/reservation"}>User Reservation</Link>
+    </div>
   );
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,24 @@
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import UserReservation from "./containers/UserReservation";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 const router = createBrowserRouter([
   {
-    path: '/',
-    element: <App />
+    path: "/",
+    element: <App />,
   },
-  // {
-  //   path: yourpathhere,
-  //   element: importyourelementhere
-  // }
-  // you also need to import Link in a different component with the 'to' property that points to your path
-  // see app.jsx for an example (dont create it in app.jsx)
-])
+  {
+    path: "/reservation",
+    element: <UserReservation />,
+  },
+]);
+{
+  /* // you also need to import Link in a different component with the 'to' property that points to your path
+  // see app.jsx for an example (dont create it in app.jsx) */
+}
 
-ReactDOM.createRoot(document.getElementById('root')).render(<RouterProvider router={router}/>)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <RouterProvider router={router} />
+);
