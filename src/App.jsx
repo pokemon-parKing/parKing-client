6509ee44e252ee87be9e8899465f66fbf3c5460f
stevkim <stevenn.kkim@gmail.com>
2023-12-27 10:34:56 -0800

    Added react-router for routing with commented out examples.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
-
+// import { Link } from 'react-router-dom';
 
 function App() {
 
   return (
     <>
+      {/* <Link to={'/pokemon'}>Pokemon</Link> */}
       parKing - Pokemon
 
       <div className='flex flex-col md:flex-row '>Test</div>
