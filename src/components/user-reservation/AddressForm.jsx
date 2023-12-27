a0c24041d477b1bf07942d4d999782b79fddd79c
Christian Lee <christianlee4037@gmail.com>
2023-12-26 20:34:35 -0800

    Added dates for date dropdown

--- src/components/user-reservation/AddressForm.jsx
+++ src/components/user-reservation/AddressForm.jsx
@@ -1,9 +1,23 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import getGeoCoordinates from "../../utils/getGeoCoordinates";
 import MapView from "./MapView";
 const AddressForm = () => {
   const [address, setAddress] = useState({});
   const [coordinates, setCoordinates] = useState(null);
+  const [date, setDate] = useState(null);
+  console.log("date", date);
+  const dates = [];
+  let currentDate = new Date();
+  for (let i = 0; i < 8; i++) {
+    let dateString = currentDate.toLocaleDateString("en-US", {
+      year: "numeric",
+      month: "2-digit",
+      day: "2-digit",
+    });
+    dates.push(<option key={dateString}>{dateString}</option>);
+    currentDate.setDate(currentDate.getDate() + 1);
+  }
+
   const GetAndSetCoordinates = async (address) => {
     try {
       const coordinates = await getGeoCoordinates(address);
@@ -15,19 +29,26 @@ const AddressForm = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    if (!address.street) return alert("Please fill address");
+    if (!date || date === "Select a date") return alert("Please select a date");
+
     const addressString = `${address.street} ${address.apt || ""}, ${
       address.city
     }, ${address.state} ${address.zip}, ${address.country}`;
     GetAndSetCoordinates(addressString);
+    // GetAvailableSpots(addressString, date);
   };
 
   return (
     <div className="flex flex-row">
-      <div className="w-1/2 border border-burgundy-p">
+      <div className="w-1/2 border-2 border-burgundy-p">
         <form onSubmit={handleSubmit}>
+          <h3 className="font-bold text-2xl">Address</h3>
           <div className="flex flex-row">
             <div className="flex flex-col w-3/4">
-              <label htmlFor="street">Street Address </label>
+              <label htmlFor="street" className="font-bold">
+                Street Address{" "}
+              </label>
               <input
                 type="text"
                 onChange={(e) => {
@@ -38,7 +59,9 @@ const AddressForm = () => {
               />
             </div>
             <div className="flex flex-col w-1/4">
-              <label htmlFor="apt">Apt/Unit # </label>
+              <label htmlFor="apt" className="font-bold">
+                Apt/Unit #{" "}
+              </label>
               <input
                 type="text"
                 onChange={(e) => {
@@ -51,7 +74,9 @@ const AddressForm = () => {
           </div>
           <div className="flex flex-row">
             <div className="flex flex-col w-1/2">
-              <label htmlFor="zip">Zip Code </label>
+              <label htmlFor="zip" className="font-bold">
+                Zip Code{" "}
+              </label>
               <input
                 type="number"
                 onChange={(e) => {
@@ -62,7 +87,9 @@ const AddressForm = () => {
               />
             </div>
             <div className="flex flex-col w-1/2">
-              <label htmlFor="city">City </label>
+              <label htmlFor="city" className="font-bold">
+                City{" "}
+              </label>
               <input
                 type="text"
                 onChange={(e) => {
@@ -75,7 +102,9 @@ const AddressForm = () => {
           </div>
           <div className="flex flex-row">
             <div className="flex flex-col w-1/2">
-              <label htmlFor="state">State </label>
+              <label htmlFor="state" className="font-bold">
+                State{" "}
+              </label>
               <input
                 type="text"
                 onChange={(e) => {
@@ -86,7 +115,9 @@ const AddressForm = () => {
               />
             </div>
             <div className="flex flex-col w-1/2">
-              <label htmlFor="country">Country </label>
+              <label htmlFor="country" className="font-bold">
+                Country{" "}
+              </label>
               <input
                 type="text"
                 onChange={(e) => {
@@ -97,7 +128,14 @@ const AddressForm = () => {
               />
             </div>
           </div>
-          <button type="submit">{!coordinates ? "Submit" : "Update"}</button>
+          <h3 className="font-bold text-2xl">Date</h3>
+          <select onChange={(e) => setDate(e.target.value)}>
+            <option>Select a date</option>
+            {dates}
+          </select>
+          <button type="submit" className="font-bold border border-burgundy-p">
+            {!coordinates ? "Submit" : "Update"}
+          </button>
         </form>
       </div>
       {coordinates && <MapView center={coordinates} />}
