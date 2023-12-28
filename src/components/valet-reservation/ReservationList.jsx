212e344777c54ec276fe6938207bed4acf3b71a5
Kyle Mak <kjmmak@gmail.com>
2023-12-27 20:58:25 -0800

    added skeleton for valetReservation page plus list, details, and spots components

---
+++ src/components/valet-reservation/ReservationList.jsx
@@ -0,0 +1,20 @@
+
+const ReservationList = () => {
+  // const { reservations } = useReservationContext();
+  return (
+    <div>
+      <h4>This is the ReservationList component</h4>
+      {/* <ul>
+        {reservations.map((reservation) => {
+          return (
+            <li key={reservation.id}>
+              {reservation.firstName} {reservation.lastName}
+            </li>
+          );
+        })}
+      </ul> */}
+    </div>
+  );
+}
+
+export default ReservationList;
