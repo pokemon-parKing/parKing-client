ac1e3af63a1ecadaa7b376f79b5088bbc97ee043
Christian Lee <christianlee4037@gmail.com>
2023-12-28 23:12:40 -0800

    Code clean up

--- src/utils/slice/reservationSlice.js
+++ src/utils/slice/reservationSlice.js
@@ -1,6 +1,7 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
+  /* Hardcoding user_id and car_id */
   reservation: { user_id: "0db22c80-80d3-46ff-a684-abddd377bd05", car_id: 1 },
 };
 
@@ -8,7 +9,6 @@ const initialState = {
 export const reservationSlice = createSlice({
   name: "reservation",
   initialState,
-  // Modify states
   reducers: {
     setDate: (state, action) => {
       state.reservation.date = action.payload;
