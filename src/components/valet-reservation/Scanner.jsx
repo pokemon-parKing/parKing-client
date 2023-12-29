85a9ec25d69028e2eb2d42a1ffd7a3202766efcb
Steven Kim <stevenn.kkim@gmail.com>
2023-12-29 11:19:39 -0800

    Update Scanner.jsx

--- src/components/valet-reservation/Scanner.jsx
+++ src/components/valet-reservation/Scanner.jsx
@@ -1,9 +1,39 @@
-
+import { useState } from ‘react’
+import { useEffect } from ‘react’;
+import { Html5QrcodeScanner } from ‘html5-qrcode’;
 const Scanner = () => {
-    return (
-        <div className="scanner">
-            <h1>Scanner</h1>
-        </div>
-    );
+  const [scanResult, setScanResult] = useState(null);
+  useEffect(() => {
+    // console.log(‘woweee!’)
+   const scanner = new Html5QrcodeScanner(‘reader’, {
+      qrbox: {
+        width: 250,
+        height: 250,
+      },
+      fps: 5,
+    });
+    scanner.render(onScanSuccess, onScanError)
+    function onScanSuccess(result) {
+      scanner.clear();
+      setScanResult(result);
+      console.log(‘Decoded QR code data: ’, result);
+      window.location.href = `http://127.0.0.1:5173/valetReservation?scanResult=${result}`;
+    }
+    function onScanError(error) {
+      console.warn(error);
+    }
+  }, []);
+  return (
+    <>
+    <div>
+      <h1>QR Code Scanner</h1>
+      <h2>scan result: {scanResult}</h2>
+      {scanResult
+      ? <div>Success: <a href={scanResult}>{scanResult}</a></div>
+      : <div id=“reader”></div>
+      }
+    </div>
+    </>
+  )
 }
 export default Scanner;
