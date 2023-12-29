0ebc169e581fc63aedbcc2f192bebb42a2ec6651
Christian Lee <christianlee4037@gmail.com>
2023-12-29 13:09:03 -0800

    Thunk WIP & Add default values for initial state as per Steven's comment

--- src/utils/slice/reservationSlice.js
+++ src/utils/slice/reservationSlice.js
@@ -1,8 +1,19 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import getGeoCoordinates from "../getGeoCoordinates";
+
+const fetchCoordinates = createAsyncThunk("reservation/getData", (address) => {
+  return getGeoCoordinates(address);
+});
 
 const initialState = {
   /* Hardcoding user_id and car_id */
-  reservation: { user_id: "0db22c80-80d3-46ff-a684-abddd377bd05", car_id: 1 },
+  reservation: {
+    user_id: "0db22c80-80d3-46ff-a684-abddd377bd05",
+    car_id: 1,
+    time: null,
+    date: null,
+    garage_id: null,
+  },
   page: "reservation",
 };
 
@@ -21,6 +32,21 @@ export const reservationSlice = createSlice({
     },
     setPage: (state, action) => {
       state.page = action.payload;
+    },
+  },
+  extraReducers: {
+    [fetchCoordinates.pending]: (state, { payload }) => {
+      state.loading = true;
+    },
+    [fetchCoordinates.fulfilled]: (state, { payload }) => {
+      state.loading = false;
+      state.data = payload;
+      state.isSuccess = true;
+    },
+    [fetchCoordinates.rejected]: (state, { payload }) => {
+      state.message = payload;
+      state.loading = false;
+      state.isSuccess = false;
     },
   },
 });
