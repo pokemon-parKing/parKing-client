77df3186ad92b14a75aa176851f0770ce64a60c9
KenMain5 <kennyize15@gmail.com>
2023-12-30 17:28:53 -0500

    initial push

---
+++ src/components/landing-page/Mainlanding.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Navbar from '../Navbar'
+
+function Mainlanding() {
+  return (
+
+    <div>
+
+        <Navbar />
+        
+    </div>
+  )
+}
+
+export default Mainlanding
