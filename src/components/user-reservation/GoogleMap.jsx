43a8130a1ca278d2762a7b4d6c6b3d324505072f
Christian Lee <christianlee4037@gmail.com>
2023-12-30 02:02:47 -0800

    Code clean up

--- src/components/user-reservation/GoogleMap.jsx
+++ src/components/user-reservation/GoogleMap.jsx
@@ -50,17 +50,7 @@ const GoogleMap = () => {
       console.log(err);
     }
   };
-  console.log(selectedGarage);
-  /*
-        "black-p": "var(--pk-black)",
-        "black-s": "var(--pk-black-secondary)",
-        "burgundy-p": "var(--pk-burgundy)",
-        "burgundy-s": "var(--pk-burgundy-secondary)",
-        "white-p": "var(--pk-white)",
-        "white-s": "var(--pk-white-secondary)",
-        "beige-p": "var(--pk-beige)",
-        "beige-s": "var(--pk-beige-secondary)",
-*/
+
   return (
     mapCenter &&
     closestGarages && (
