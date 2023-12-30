be61f33cc4c8145d6a1a4bd1c73e9626e2489980
Christian Lee <christianlee4037@gmail.com>
2023-12-30 13:10:16 -0800

    Add magnifying glass

--- src/components/user-reservation/MapView.jsx
+++ src/components/user-reservation/MapView.jsx
@@ -11,13 +11,11 @@ const MapView = () => {
     <div className="flex flex-row w-full">
       <div className="flex flex-col w-full items-center">
         <h3 className="text-2xl pb-5 pt-5">Reserve your spot!</h3>
-
         <MapSideBar />
         <div className="divider pt-5"></div>
         {reservationsList && (
           <div className="flex flex-col items-center pt-5">
             <h3 className="pb-3 text-2xl">{selectedGarage.name}</h3>
-
             <TimeSlotList />
           </div>
         )}
--- src/components/user-reservation/SearchBar.jsx
+++ src/components/user-reservation/SearchBar.jsx
@@ -24,11 +24,27 @@ const SearchBar = () => {
   };
 
   return (
-    <form onSubmit={handleSubmit} className="w-3/4">
+    <form onSubmit={handleSubmit} className="flex flex-row items-center w-3/4">
+      <div className="mr-[-32px] z-10">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className="w-6 h-6"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z"
+          />
+        </svg>
+      </div>
       <div className="join flex justify-center w-full">
         <input
           required
-          className="input input-bordered join-item w-full"
+          className="input input-bordered join-item pl-10 w-full"
           placeholder="Address, Place, City or Venue"
           type="text"
           onChange={(e) => {
