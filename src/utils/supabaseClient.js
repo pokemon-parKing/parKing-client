15941ef1a471680df1620f686e2ac8946b34c1d4
Steven Kim <stevenn.kkim@gmail.com>
2023-12-31 16:07:13 -0800

    Delete src/utils/supabaseClient.js
    Needed to delete specific sensitive info

--- src/utils/supabaseClient.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-const supabaseUrl = 'https://iibwbjdisltiujjuglkp.supabase.co';
-const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRqbGdiY21hYmxuZmRzdGVoaXRlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDI5MjEyOTYsImV4cCI6MjAxODQ5NzI5Nn0.UP9wJ9hugJMWOXJdwVYdwjaH0o5eRNTdQN2PqAoY9s8"; // Set to null to protect credentials, dev to replace with anonymous key
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
