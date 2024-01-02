6abdb0cbcd8ac0f4ec4d68f6a216b3676c3e5807
kssampson <kylesampsonmusic@gmail.com>
2024-01-01 21:58:57 -0800

    commented useEffects in reservationList component and set up useEffect in reservationDetails, which is now showing data from the useParams on render. Added check in/out buttons but are non operational for now

--- src/components/valet-reservation/ReservationDetails.jsx
+++ src/components/valet-reservation/ReservationDetails.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react';
-import { useSelector, useDispatch } from "react-redux";
+// import { useSelector, useDispatch } from "react-redux";
 // import { setReservationDetails } from "../../utils/slice/valetSlice";
+import getReservationDetails from "../../utils/getReservationDetails";
 import { useParams } from 'react-router-dom';
 import { Link } from "react-router-dom";
 
@@ -9,29 +10,71 @@ const ReservationDetails = () => {
   const { reservation_id } = useParams()
 
   const [reservation, setReservation] = useState({
-    reservation_id: reservation_id,
-    reservationStatus: '',
-    firstName: '',
-    lastName: '',
-    email: '',
-    carId: 0,
-    licensePlate: '',
-    carMake: '',
-    carModel: '',
-    carColor: '',
-    timeIn: '',
-    timeOut: '',
-    parkingSpot: ''
+    accounts: {
+      email: "",
+      first_name: "",
+      last_name: "",
+      phone_number: "",
+    },
+    cars: {
+      color: "",
+      license_plate_number: "",
+      make: "",
+      model: "",
+    },
+    date: "",
+    id: 0,
+    parking_spot_id: 0,
+    time: 0,
   });
 
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const data = await getReservationDetails(reservation_id);
+        setReservation(data[0]);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    fetchData();
+  }, [reservation_id])
+
   return (
-    <>
-      <div>
+      <div className="reservation-details-container">
       <Link to="/valetReservation">Back to Valet Reservation Page</Link>
-        <h1>Reservation Details</h1>
-        <h1>reservation_id pulled from params on render: {reservation_id}</h1>
+      <br/>
+      <br/>
+      <div className="reservation-details">
+        <h3>Reservation Details</h3>
+        <p>Reservation ID: {reservation.id}</p>
+        <p>Date: {reservation.date}</p>
+        <p>Parking Spot ID: {reservation.parking_spot_id}</p>
+        <p>Time: {reservation.time}</p>
       </div>
-    </>
+
+      <div className="account-details">
+        <h3>Account Information</h3>
+        <p>Email: {reservation.accounts.email}</p>
+        <p>Name: {`${reservation.accounts.last_name}, ${reservation.accounts.first_name}`}</p>
+        <p>Phone Number: {reservation.accounts.phone_number}</p>
+      </div>
+
+      <div className="car-details">
+        <h3>Car Information</h3>
+        <p>Color: {reservation.cars.color}</p>
+        <p>License Plate Number: {reservation.cars.license_plate_number}</p>
+        <p>Make: {reservation.cars.make}</p>
+        <p>Model: {reservation.cars.model}</p>
+      </div>
+      <br/>
+      <br/>
+
+      <button className="check-in-button">Check In</button>
+      <br/>
+      <br/>
+      <button className="check-out-button">Check out</button>
+    </div>
   );
 }
 
