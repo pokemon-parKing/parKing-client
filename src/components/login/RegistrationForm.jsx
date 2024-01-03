89ff5fb89992d5b55aad746c138f0e7a113a06b1
Jose Felix <jmfelix195@gmail.com>
2024-01-03 16:28:53 -0700

    Added styles to registration forms

--- src/components/login/RegistrationForm.jsx
+++ src/components/login/RegistrationForm.jsx
@@ -98,12 +98,28 @@ const RegistrationForm = ({ role }) => {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <AccountForm formData={formData} handleChange={handleChange} />
-      {role === 'driver' ? <VehicleForm formData={formData} handleChange={handleChange} /> : null}
-      {role === 'valet' ? <GarageForm formData={formData} handleChange={handleChange} /> : null}
-      <button type="submit">Register</button>
-    </form>
+    <div className="xl:max-w-7xl bg-white drop-shadow-xl border border-black/20 w-full rounded-md flex justify-between items-stretch px-5 xl:px-5 py-5">
+      <div className="mx-auto w-full lg:w-1/2 md:p-10 py-5 md:py-0">
+        <h1 className="text-center text-2xl sm:text-3xl font-semibold text-[#000]">
+          Tell us about yourself
+        </h1>
+        <div className="w-full mt-5 sm:mt-8">
+          <div className={`mx-auto w-full sm:max-w-md md:max-w-lg flex flex-col gap-5 h-[${role === 'driver' ? 700 : 1000}px] overflow-y-auto`}>
+        <form onSubmit={handleSubmit}>
+          <AccountForm formData={formData} handleChange={handleChange} />
+          {role === 'driver' ? <VehicleForm formData={formData} handleChange={handleChange} /> : null}
+          {role === 'valet' ? <GarageForm formData={formData} handleChange={handleChange} /> : null}
+          <div className="flex flex-col md:flex-row gap-2 md:gap-4 justify-center items-center">
+            <button
+              type="submit"
+              className="btn btn-active bg-black border-black text-white btn-primary btn-block max-w-[200px]"
+            >Register</button>
+          </div>
+        </form>
+      </div>
+      </div>
+      </div>
+    </div>
   );
 };
 
