24450c0fbb7d3a930bf056c94d45159b63d858d0
Kyle Mak <kjmmak@gmail.com>
2024-01-04 11:04:55 -0800

    updated vite env import

--- src/utils/getReservationsByDate.js
+++ src/utils/getReservationsByDate.js
@@ -1,11 +1,10 @@
 import axios from "axios";
-import { VITE_RESERVATION_HOST } from "../env.js";
 
 const getReservationsByDate = async (garage_id, date) => {
   /* Given garage_id and date, get count of reservations per hour */
   try {
     const { data } = await axios.get(
-      `${VITE_RESERVATION_HOST}/reservations/garage/${garage_id}?date=${date}`
+      `${import.meta.env.VITE_RESERVATION_HOST}/reservations/garage/${garage_id}?date=${date}`
     );
     return data;
   } catch (error) {
--- src/utils/valetReservationUtils.js
+++ src/utils/valetReservationUtils.js
@@ -1,10 +1,9 @@
 import axios from "axios";
-import { VITE_RESERVATION_HOST } from "../env.js";
 
 const getQrCode = async (reservationId) => {
   try {
     const linkToImage = await axios.get(
-      `${VITE_RESERVATION_HOST}/getqr/${reservationId}`
+      `${import.meta.env.VITE_RESERVATION_HOST}/getqr/${reservationId}`
     );
     console.log("link to image generated: ", linkToImage.data.publicUrl);
     return linkToImage.data.publicUrl;
@@ -18,7 +17,7 @@ const getReservationDetails = async (reservation_id) => {
   /* Given reservation_id, get reservation details */
   try {
     const { data } = await axios.get(
-      `${VITE_RESERVATION_HOST}/reservations/valet/detail/${reservation_id}`
+      `${import.meta.env.VITE_RESERVATION_HOST}/reservations/valet/detail/${reservation_id}`
     );
     return data;
   } catch (error) {
@@ -31,7 +30,7 @@ const getReservationList = async (garage_id, date) => {
   /* Given garage_id and date, get count of reservations per hour */
   try {
     const { data } = await axios.get(
-      `${VITE_RESERVATION_HOST}/reservations/valet/list/${garage_id}?date=${date}`
+      `${import.meta.env.VITE_RESERVATION_HOST}/reservations/valet/list/${garage_id}?date=${date}`
     );
     return data;
   } catch (error) {
@@ -44,7 +43,7 @@ const getSpotMetrics = async (garage_id, date, time) => {
   /* Given garage_id, date, and time, get information about the number of occupied(checked-in), reserved, and available spots */
   try {
     const { data } = await axios.get(
-      `${VITE_RESERVATION_HOST}/reservations/valet/${garage_id}?date=${date}&time=${time}`
+      `${import.meta.env.VITE_RESERVATION_HOST}/reservations/valet/${garage_id}?date=${date}&time=${time}`
     );
     return data;
   } catch (error) {
@@ -56,7 +55,7 @@ const getSpotMetrics = async (garage_id, date, time) => {
 const postGenerateQrCode = async (reservationId) => {
   try {
     const response = await axios.post(
-      `${VITE_RESERVATION_HOST}/qr/${reservationId}`
+      `${import.meta.env.VITE_RESERVATION_HOST}/qr/${reservationId}`
     );
     console.log("QR code generated");
     return response.data;
@@ -69,7 +68,7 @@ const postGenerateQrCode = async (reservationId) => {
 const updateStatus = async (status, reservationId) => {
   try {
     await axios.patch(
-      `${VITE_RESERVATION_HOST}/reservations/${reservationId}?status=${status}`
+      `${import.meta.env.VITE_RESERVATION_HOST}/reservations/${reservationId}?status=${status}`
     );
   } catch (error) {
     console.log(error);
