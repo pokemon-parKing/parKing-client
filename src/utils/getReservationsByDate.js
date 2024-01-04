b0e5d76092bf771f9738f7cef4ccd06501ed6a9b
bzw24 <brucewong24@gmail.com>
2024-01-04 11:00:23 -0800

    edited host key merge conflict

--- src/utils/getReservationsByDate.js
+++ src/utils/getReservationsByDate.js
@@ -5,7 +5,7 @@ const getReservationsByDate = async (garage_id, date) => {
   /* Given garage_id and date, get count of reservations per hour */
   try {
     const { data } = await axios.get(
-      `${VITE_RESERVATION_HOST}/reservations/garage/${garage_id}?date=${date}`
+      `${HOST}/reservations/garage/${garage_id}?date=${date}`
     );
     return data;
   } catch (error) {
