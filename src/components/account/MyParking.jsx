59e1f39caac7f2cab43202ca6346c0edb4bbe90e
bzw24 <brucewong24@gmail.com>
2024-01-04 13:42:10 -0800

    card no no longer needs to change size

--- src/components/account/MyParking.jsx
+++ src/components/account/MyParking.jsx
@@ -45,7 +45,7 @@ const MyParking = () => {
           {reservationData.map((reservation) => (
             <div
               key={reservation.id}
-              className="card bg-base-100 shadow-xl mb-3 w-[300px] sm:w-[300px] h-[300px] sm:h-[300px]"
+              className="card bg-base-100 shadow-xl mb-3 w-[300px] h-[300px]"
             >
               <div className="card-body items-center text-center">
                 <h2 className="card-title">Reservation Information</h2>
@@ -83,7 +83,7 @@ const MyParking = () => {
             </div>
           ))}
           <div
-            className="card bg-base-100 shadow-xl mb-3 w-[300px] sm:w-[300px] h-[300px] sm:h-[300px] hover:shadow-2xl transform hover:scale-105 transition-transform hover:cursor-pointer"
+            className="card bg-base-100 shadow-xl mb-3 w-[300px] h-[300px] hover:shadow-2xl transform hover:scale-105 transition-transform hover:cursor-pointer"
             onClick={handleAddNew}
           >
             <div className="card-body flex flex-col justify-center items-center text-center h-full">
