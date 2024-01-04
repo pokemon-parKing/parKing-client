1bfa60a3766ed1e585443faca076cd065c53b179
Jose Felix <jmfelix195@gmail.com>
2024-01-04 13:46:24 -0700

    Change style to center form

--- src/components/login/RegistrationForm.jsx
+++ src/components/login/RegistrationForm.jsx
@@ -98,7 +98,7 @@ const RegistrationForm = ({ role }) => {
   };
 
   return (
-    <div className="xl:max-w-7xl bg-white drop-shadow-xl border border-black/20 w-full rounded-md flex justify-between items-stretch px-5 xl:px-5 py-5">
+    <div className="xl:max-w-7xl bg-white drop-shadow-xl border border-black/20 w-full rounded-md flex justify-between items-stretch px-5 xl:px-5 py-5 mx-auto">
       <div className="mx-auto w-full lg:w-1/2 md:p-10 py-5 md:py-0">
         <h1 className="text-center text-2xl sm:text-3xl font-semibold text-[#000]">
           Tell us about yourself
